760c5c78f1938842ac2cc362c438856449030426
Roma <Roma@Roma>
2018-08-03 23:55:55 +0300

    Merge branch 'master' of https://github.com/romatym/Netology

---
+++ config/DbConfig.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'type' => 'mysql',
+    'host' => 'localhost',
+    'dbname' => 'rtymtsiv',
+    'charset' => 'utf8',
+    'user' => 'rtymtsiv',
+    'pass' => 'neto1754'
+];
